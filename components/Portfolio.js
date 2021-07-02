8705a3048ee28776df2b76b4e4fd7880c411fdb1
fraan83 <fraan83@gmail.com>
2021-07-02 09:57:52 +0200

    first commit

---
+++ components/Portfolio.js
@@ -0,0 +1,191 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import styles from '../styles/Portfolio.module.css';
+import PersonIcon from '@material-ui/icons/Person';
+import MailOutlineIcon from '@material-ui/icons/MailOutline';
+import PinDropIcon from '@material-ui/icons/PinDrop';
+import PhoneIcon from '@material-ui/icons/Phone';
+import LockOpenIcon from '@material-ui/icons/LockOpen';
+
+const Portfolio = () => {
+
+
+
+
+    const [utente, setUtente] = useState({
+        gender: '', // 
+        name: {
+            title: '',
+            first: '',
+            last: ''
+        },
+        location: {
+            street: {
+                number: 0,
+                name: ''
+            },
+            city: '',
+            state: '',
+            country: '',
+            postcode: 66666,
+            coordinates: {
+                langitude: '',
+                longitude: ''
+            },
+            timezone: {
+                offset: '',
+                description: ''
+            }
+        },
+        email: '', //
+        login: {
+            uuid: '',
+            username: '',
+            password: '',
+            salt: '',
+            md5: '',
+            sha1: '',
+            sha512: ''
+        },
+        dob: {
+            date: '',
+            age: 0
+        }, //
+        registered: {
+            date: '',
+            age: 0
+
+        }, //
+        phone: '',
+        cell: '',
+        id: {
+            name: '',
+            value: null
+        },
+        picture: {
+            large: '',
+            medium: '',
+            thumbnail: ''
+        },
+        nat: ''
+    })
+
+    useEffect(() => {
+        axios.get('https://randomuser.me/api')
+            .then(res => {
+                console.log(res.data.results[0]);
+                let user = res.data.results[0]
+                setUtente(user);
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }, [])
+
+    const [isShown, setIsShown] = useState(false);
+    const [info, setInfo] = useState('');
+    const [titolo, setTitolo] = useState('');
+    const mostraDettagli = (e) => {
+        console.log('event', e.target.name)
+        setIsShown(true);
+        if (e.target.name === 'name') {
+            setTitolo('Nome e Cognome')
+            setInfo(`${utente.name.title + ' ' + utente.name.first + ' ' + utente.name.last}`)
+        } else if (e.target.name === 'email') {
+            setTitolo('Email')
+            setInfo(`${utente.email}`)
+        } else if (e.target.name === 'phone') {
+            setTitolo('Telefono')
+            setInfo(`${utente.cell}`)
+        } else if (e.target.name === 'location') {
+            setTitolo('Indirizzo')
+            setInfo(`${utente.location.street.name + '\n' + utente.location.street.number + '\n' + utente.location.city + ' ' + utente.location.state}`)
+        } else if (e.target.name === 'password') {
+            setTitolo('Password')
+            setInfo(`${utente.login.password}`)
+        }
+    }
+
+
+
+    return (
+        <div className="container d-flex" style={{ border: '1px solid lightgrey', borderRadius: '6px' }}>
+            <div className={styles.content}>
+                <picture className={styles.containerImg}>
+                    <img className={styles.roundedImg} src={utente.picture.medium} alt={utente.name.first + ' ' + utente.name.last} title={utente.name.first + ' ' + utente.name.last} />
+                    <div className={styles.overlayImg}>
+                        <img src={utente.picture.large} ></img>
+                    </div>
+                </picture>
+            </div>
+
+
+            <div className={styles.flexContainer}>
+
+                <div className={styles.flexItem}>
+                    {isShown && (
+
+                        <div className={styles.textToshow}>
+
+                            <h3>{titolo}</h3>
+                            <br />
+                            {info}
+                        </div>
+                    )}
+                </div>
+
+                <div className={styles.flexItem}>
+                    <ul className={styles.listaIcone}>
+                        <li>
+                            <button
+                                name="name"
+                                onMouseEnter={(e) => mostraDettagli(e)}
+                            >
+                                <PersonIcon />
+                            </button>
+
+                        </li>
+                        <li>
+                            <button
+                                name="email"
+                                onMouseEnter={(e) => mostraDettagli(e)}
+                            >
+                                <MailOutlineIcon />
+                            </button>
+
+                        </li>
+                        <li>
+                            <button
+                                name="phone"
+                                onMouseEnter={(e) => mostraDettagli(e)}
+                            >
+                                <PhoneIcon />
+                            </button>
+
+
+                        </li>
+                        <li>
+                            <button
+                                name="location"
+                                onMouseEnter={(e) => mostraDettagli(e)}
+                            >
+                                <PinDropIcon />
+                            </button>
+                        </li>
+                        <li>
+                            <button
+                                name="password"
+                                onMouseEnter={(e) => mostraDettagli(e)}
+                            >
+                                <LockOpenIcon />
+                            </button>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default Portfolio
---
+++ pages/_documnet.js
@@ -0,0 +1,28 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html>
+       <Head>
+       <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossOrigin="anonymous" />
+
+           </Head>
+        <body>
+          <Main />
+          <NextScript />
+          <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossOrigin="anonymous"></script>
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
